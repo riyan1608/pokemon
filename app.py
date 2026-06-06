fc71146a72ffe6c9ccdbee7a5590a4704a554df8
riyan1608 <riyansika.s@gmail.com>
2026-06-06 17:44:33 +1000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import requests
+st.header("Pokemon Images")
+mypokemon=['charizard','pikachu','eevee','snorlax','garchomp','lucario']
+selection = st.selectbox("Select your Pokemon", mypokemon)
+if selection:
+    r=requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon}').json()
+    for img in r['sprites'].values():
+        if img is not None:
+            if str(img)[-4:]=='.png':
+                st.image(img)
