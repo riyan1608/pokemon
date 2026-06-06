7ee1e1391e6102333d3c72ffeb17873fad02fd77
riyan1608 <riyansika.s@gmail.com>
2026-06-06 17:47:16 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.header("Pokemon Images")
 mypokemon=['charizard','pikachu','eevee','snorlax','garchomp','lucario']
 selection = st.selectbox("Select your Pokemon", mypokemon)
 if selection:
-    r=requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon}').json()
+    r=requests.get(f'https://pokeapi.co/api/v2/pokemon/{selection}').json()
     for img in r['sprites'].values():
         if img is not None:
             if str(img)[-4:]=='.png':
